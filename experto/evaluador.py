ce112533dfdc4ff2d6ef917fc5f04cf8166cdff3
exe <exequielbrites8@gmail.com>
2025-11-28 02:12:16 -0300

    feat:agregar un modo al evaluador.simplificando la informacion obtenida

--- experto/evaluador.py
+++ experto/evaluador.py
@@ -2,17 +2,38 @@
 from experto.motor import MotorNutricional, Nutricion, cargar_reglas
 
 
-def evaluar_nutricion(data):
+def evaluar_nutricion(data, modo="completo"):
     engine = MotorNutricional()
     cargar_reglas(engine)
 
     engine.reset()
     engine.declare(Nutricion(**data))
     engine.run()
-
-    return {
+    # Resultado general
+    resultado = {
+        "errores": engine.errores,
+        "advertencias_validacion": engine.advertencias_validacion,
+        "info": engine.info,
         "riesgos": engine.riesgos,
-        "advertencias": engine.advertencias,
+        "advertencias_nutricionales": engine.advertencias_nutricionales,
         "beneficios": engine.beneficios,
         "clasificacion": engine.clasificacion,
     }
+    # ---- FILTRADOS POR MODO ---- #
+
+    if modo == "crud":
+        return {
+            "errores": engine.errores,
+            "advertencias": engine.advertencias_validacion,
+            "info": engine.info,       # si querés se puede remover
+        }
+
+    if modo == "nutricional":
+        return {
+            "riesgos": engine.riesgos,
+            "advertencias": engine.advertencias_nutricionales,
+            "beneficios": engine.beneficios,
+            "clasificacion": engine.clasificacion,
+        }
+
+    return resultado   # modo completo
