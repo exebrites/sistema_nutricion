81d27f6c627286eb192512e2fcfe7dea8c0f2de8
exe <exequielbrites8@gmail.com>
2025-11-29 23:42:04 -0300

    feat: super admin y registro de modelos

--- nutricion/admin.py
+++ nutricion/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from nutricion.models import Alimento, Antropometria, Dieta, DietaAlimento, HabitoAlimentario, MotivoConsulta, Paciente, Sintoma
+
 # Register your models here.
+admin.site.register(Alimento)
+admin.site.register(Dieta)
+admin.site.register(DietaAlimento)
+admin.site.register(Paciente)
+admin.site.register(Antropometria)
+admin.site.register(MotivoConsulta)
+admin.site.register(Sintoma)
+admin.site.register(HabitoAlimentario)
